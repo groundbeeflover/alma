95ae050ffd8085848a89e4b4b28bd903d1ecf024
groundbeeflover <gevegg22@gmail.com>
2025-02-23 20:52:40 +0100

    auth incomplete

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 
 const Page = () => {
+  // throw new Error("Error in home page");
   return(
     <div>
       <h1 className="text-2xl"> home </h1>
+
     </div>
 
   )
